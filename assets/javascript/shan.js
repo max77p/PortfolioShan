bf003a8a29d28d0166bcfabe5b125655a0e264a3
max77p <shanindrakumar@gmail.com>
2018-06-06 20:58:08 -0400

    added typed

---
+++ assets/javascript/shan.js
@@ -0,0 +1,24 @@
+
+// $(document).ready(function () {
+//     var typedOptions = {
+//         strings: ["Shan.", "Front-End.", "Back-End.", "Creative."],
+//         typeSpeed: 100,
+//         backSpeed: 50,
+//         loop: true
+//     }
+//     var typed = new Typed(".typed", typedOptions);
+
+// });
+
+
+
+setTimeout(function () {
+    var typedOptions = {
+        strings: ["Shan.", "Front-End.", "Back-End.", "Creative."],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+    }
+    var typed = new Typed(".typed", typedOptions);
+
+}, 1000);
